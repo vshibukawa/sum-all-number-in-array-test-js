f20c08bba4b24d7c543089089f3a0f79d9c3b106
vshibukawa <viviane.shibukawa@gmail.com>
2019-04-30 11:54:07 +0000

    updated

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+
+  let sum = 0;
+
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      return sum += sumItems(item);
+    } else {
+      return sum += item;
+    }
+  });
+
+  return sum;
 }
 
 module.exports = sumItems;
